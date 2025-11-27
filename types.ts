c0d7b08f9af6da431d7ddee5a4e86e874bf83479
Akkurakey <92529866+Akkurakey@users.noreply.github.com>
2025-11-27 03:36:42 +0000

    Update types.ts

--- types.ts
+++ types.ts
@@ -9,7 +9,8 @@ export enum SlideId {
   ARCHITECTURE = 3,
   DEMO = 4,
   PARTNERSHIPS = 5,
-  REQUIREMENTS = 6,
-  IMPACT = 7,
-  FUTURE = 8,
+  CASES = 6
+  REQUIREMENTS = 7,
+  IMPACT = 8,
+  FUTURE = 9,
 }
